2d7f3b430051eae1de48dbaadf373be78b3f3311
Neko705 <129487693+Neko705@users.noreply.github.com>
2023-12-01 12:49:58 +0530

    commit

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import './navbar.css'
+import logo from './youtube.ico'
 
 function Navbar(){
     return(
-        <div>NAVBAR</div>
+        <div className="container_navbar">
+            <div className="Burger_logo_navbar">
+                <div className="burger">
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                </div>
+                <div className="logo_div_navbar">
+                    <img src={logo} alt="" />
+                    <p className="logo_title_navbar">YouTube</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
