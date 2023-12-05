bfa7df2f3c097ea1d0b23483de37c32cac9e5d3f
Neko705 <129487693+Neko705@users.noreply.github.com>
2023-12-05 13:42:59 +0530

    commit

--- client/src/components/Navbar/SearchBar/searchbar.jsx
+++ client/src/components/Navbar/SearchBar/searchbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './searchbar.css';
 import {FaSearch} from 'react-icons/fa';
+import {BsMicFill} from 'react-icons/bs'
 
 function Searchbar() {
   return (
@@ -8,8 +9,9 @@ function Searchbar() {
       <div className='SearchBar_container'>
         <div className='SearchBar_container2'>
             <div className='search_div'>
-                <input type="text" className='iBox_SearchBar'/>
+                <input type="text" className='iBox_SearchBar' placeholder='Search..'/>
                 <FaSearch className='searchIcon_Searchbar'/>
+                <BsMicFill className='mic_Searchbar'/>
             </div>
         </div>
       </div>
--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import './navbar.css'
 import logo from './youtube.ico'
 import Searchbar from "./SearchBar/searchbar";
+import {RiVideoAddLine} from 'react-icons/ri';
+import {IoMdNotificationsOutline} from 'react-icons/io';
+import {BiUserCircle} from 'react-icons/bi'
+
 
 function Navbar(){
+    const CurrentUser=null;
     return(
         <div className="container_navbar">
             <div className="Burger_logo_navbar">
@@ -18,6 +23,13 @@ function Navbar(){
                 </div>
             </div>
             <Searchbar />
+            <RiVideoAddLine size={24} className={"vid_bell_Navbar"}/>
+            <IoMdNotificationsOutline size={24} className="vid_bell_Navbar"/>
+            <div className="User_cont_navbar">
+                <p className="User_btn">
+                    <BiUserCircle size={40} aria-placeholder="Sign In"/>
+                </p>
+            </div>
         </div>
     )
 }
